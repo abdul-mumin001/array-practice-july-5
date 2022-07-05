d36a5aa250af5b6757103630520af0ab23f6ad5b
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-07-05 22:41:59 +0530

    array

---
+++ array/array.js
@@ -0,0 +1,62 @@
+// let names=['vasant','mumin','abdul','zia']
+
+// let name='mumin'
+// let age=22;
+// let location='Silchar'
+
+
+
+// let Rajnifamily= {
+//     name : 'Rajni',
+//     age:75,
+//     daughter: {
+//         name: 'Ishwarya',
+//         age:40,
+
+//     }
+// }
+// // console.log(Rajnifamily.daughter.name)
+// console.log(Rajnifamily['daughter']['name'])
+
+
+
+
+
+
+
+//  let names=['vasant','mumin','abdul','zia']
+
+//  console.log(names[1])
+
+
+
+// let names=['mumin',10,true,{
+//     name:'abdul'
+// },]
+
+
+// let people= {
+//     0: 'mumin',
+//     1: 'sabul',
+//     2: 'rumi'
+// }
+
+// people=['mumin','sabul','rumi']
+
+// console.log(names)
+
+
+
+
+
+// people=['mumin','sabul','rumi']
+// people[1]='mumu'
+// console.log(people[0],people[1],people[2])
+
+
+
+let num=[num,copy];
+num=[10,20]
+console.log(num,copy)
+copy[0]=15
+console.log(num,copy)
