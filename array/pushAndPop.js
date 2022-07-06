4d3383e2a304602d4948ede741911a910f4ae775
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-07-06 22:18:01 +0530

    array

---
+++ array/pushAndPop.js
@@ -0,0 +1,44 @@
+// Print first element and last element
+
+// let nums=[1,2,3,4,5,6,7]
+
+// console.log(nums[0],nums[nums.length-1])
+
+// // Print all element
+
+// let arrayLength=nums.length;
+// for(let index=0;index<arrayLength;index++){
+//     console.log(nums[index])
+// }
+
+let nums=[1,2,3,4,5,6,7]
+
+// console.log('initial',nums)
+
+// // if we want to add then we use .push 
+
+// nums.push('mumin')
+
+// console.log('adding',nums)
+
+// // if we want to dlt then we use .pop 
+
+// console.log(nums.pop())
+
+// console.log('popping',nums)
+
+
+// nums.pop()
+// nums.pop()
+// nums.pop()
+// nums.pop()
+// nums.pop()
+// nums.pop()
+// nums.pop()
+// console.log('popping',nums)
+
+nums.push(1)
+console.log(nums)
+
+
+
